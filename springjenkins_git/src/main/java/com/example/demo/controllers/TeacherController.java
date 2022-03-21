05a47f40225c00a92994ffcc94df8781bf832f36
Venkataramana.R <Venkataramana.R@EXIND3010-118.allfon.in>
2022-03-21 12:48:31 +0530

    new method added

--- springjenkins_git/src/main/java/com/example/demo/controllers/TeacherController.java
+++ springjenkins_git/src/main/java/com/example/demo/controllers/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,16 @@ public class TeacherController {
 		List<String> list =Arrays.asList("amar","akbar","anthony");
 		return list.toString();
 	}
+	@GetMapping(path = "/teachers/{dept}")
+	public String getTeachersByDept(@PathVariable("dept") String dept) {
+		
+		List<String> list =Arrays.asList("amar","akbar","anthony");
+		
+		if(dept.equals("ece")) {
+			list = Arrays.asList("shankar","shyaam","sion");
+		}
+		return list.toString();
+	
 
+}
 }
